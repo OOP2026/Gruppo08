cb738dbd6152df773717046630d2b412a7927ef3
andryrob06-glitch <andryrob06@gmail.com>
2026-05-28 11:43:19 +0200

    work in progress (orario)

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -12,19 +12,19 @@ public class Controller {
 
 	private Utente session;
 
-	public boolean isCoordinatore(Utente session) {
+	public boolean isCoordinatore() {
 		if (session.puoModificareOrario() && session.puoRichiedereSpostamento())
 			return true;
 		return false;
 	}
 
-	public boolean isDocente(Utente session) {
+	public boolean isDocente() {
 		if (!session.puoModificareOrario() && session.puoRichiedereSpostamento())
 			return true;
 		return false;
 	}
 
-	public boolean isStudente(Utente session) {
+	public boolean isStudente() {
 		if (!session.puoModificareOrario() && !session.puoRichiedereSpostamento())
 			return true;
 		return false;
@@ -75,28 +75,28 @@ public class Controller {
 	 **********************
 	 */
 	public boolean makeInsegnamento(Materia materia, int numeroCfu, int annoDiCorso) {
-		if (!isCoordinatore(session))
+		if (!isCoordinatore())
 			return false;
 		sUni.makeInsegnamento(materia, numeroCfu, annoDiCorso);
 		return true;
 	}
 
 	public boolean makeAnnoAccademico(int anno) {
-		if (!isCoordinatore(session))
+		if (!isCoordinatore())
 			return false;
 		sUni.makeAnnoAccademico(anno);
 		return true;
 	}
 
 	public boolean makeAnnoAccademico(int anno, Insegnamento... insegnamenti) {
-		if (!isCoordinatore(session))
+		if (!isCoordinatore())
 			return false;
 		sUni.makeAnnoAccademico(anno, insegnamenti);
 		return true;
 	}
 
 	public boolean makeAula(char lettera, int numero, int capacita) {
-		if (!isCoordinatore(session))
+		if (!isCoordinatore())
 			return false;
 		sUni.makeAula(lettera, numero, capacita);
 		return true;
@@ -109,14 +109,14 @@ public class Controller {
 	 */
 
 	public boolean makeLezione(Aula aula, Insegnamento insegnamento, LocalDateTime oraInizio, LocalDateTime oraFine) {
-		if (!isCoordinatore(session))
+		if (!isCoordinatore())
 			return false;
 		sLezioni.makeLezione(aula, insegnamento, oraInizio, oraFine);
 		return true;
 	}
 
 	public boolean makeOrarioLezione(AnnoAccademico anno, DayOfWeek giornoSett, Insegnamento insegnamento) {
-		if (!isCoordinatore(session))
+		if (!isCoordinatore())
 			return false;
 		sLezioni.makeOrarioLezione(anno, giornoSett, insegnamento);
 		return true;
