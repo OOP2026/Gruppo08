3664dc6e877be3c6647af63ef67a1efc1cd61323
alessio <mannaalessio@tutamail.com>
2026-06-27 21:06:43 +0200

    vincoli di integrita' semantica
    e fix di <> null al posto di is not null

--- doc/documentazioneFinale/schema.sql
+++ doc/documentazioneFinale/schema.sql
@@ -1,7 +1,6 @@
 CREATE USER exam WITH PASSWORD 'exam';
 CREATE DATABASE exam OWNER exam;
 
--- TODO: vincolo di integrita' semantica: un app_user non puo' essere senza ruolo e non possono esistere uno student e un teacher con lo stesso user_id. 
 CREATE TABLE app_user (
 	user_id serial PRIMARY KEY, 
 	fname varchar(64) NOT NULL,
@@ -58,7 +57,6 @@ CREATE TABLE lecture (
 
 CREATE TYPE request_status AS ENUM ('WAITING', 'APPROVED', 'REJECTED');
 
--- TODO: se status <> 'WAITING' allora reviewing_coord_id deve essere <> null
 CREATE TABLE change_of_date_req (
 	req_id serial PRIMARY KEY,
 
@@ -73,11 +71,38 @@ CREATE TABLE change_of_date_req (
 
 	UNIQUE(lecture_id, new_dayofweek, new_start_time),
     CONSTRAINT chk_times CHECK (new_end_time > new_start_time),
-	CONSTRAINT chk_status_integrity CHECK (status = 'WAITING' OR reviewing_coord_id <> null)
+	CONSTRAINT chk_status_integrity CHECK (status = 'WAITING' OR reviewing_coord_id is not null)
 );
 
--- TODO: trigger per controllare che un professore non abbia due lezioni nello stesso intervallo orario
 
+-- non puo' essere creato un utente con due ruoli
+CREATE OR REPLACE FUNCTION check_user_role_integrity()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_TABLE_NAME = 'student' THEN
+        IF EXISTS (SELECT 1 FROM teacher WHERE user_id = NEW.user_id) THEN
+            RAISE EXCEPTION 'Error: user % is already registered as teacher.', NEW.user_id;
+        END IF;
+
+    ELSIF TG_TABLE_NAME = 'teacher' THEN
+        IF EXISTS (SELECT 1 FROM student WHERE user_id = NEW.user_id) THEN
+            RAISE EXCEPTION 'Error: user % is already registered as student.', NEW.user_id;
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_assure_user_role_integrity_student
+BEFORE INSERT OR UPDATE OF user_id ON student
+FOR EACH ROW EXECUTE FUNCTION check_user_role_integrity();
+
+CREATE TRIGGER trg_assure_user_role_integrity_teacher
+BEFORE INSERT OR UPDATE OF user_id ON teacher
+FOR EACH ROW EXECUTE FUNCTION check_user_role_integrity();
+
+-- una lezione puo' essere programmata solo se l'aula non e' gia' occupata nella data e nell'orario stabiliti
 CREATE OR REPLACE FUNCTION fn_assure_no_lecture_overlapping()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -86,11 +111,8 @@ BEGIN
 		FROM lecture
 		WHERE classroom_name = new.classroom_name
 		AND dayofweek = new.dayofweek
-		AND (
-			new.start_time BETWEEN start_time AND end_time
-			OR
-			new.end_time BETWEEN start_time AND end_time
-		)
+		AND (TG_OP = 'INSERT' OR lecture_id <> new.lecture_id) -- Fix Update
+		AND (new.start_time < end_time AND new.end_time > start_time) -- Fix Overlap Temporale
 	) THEN
 		RAISE EXCEPTION 'classroom is already busy on % %-%', new.dayofweek, new.start_time, new.end_time;
 	END IF;
@@ -104,6 +126,39 @@ BEFORE INSERT OR UPDATE OF classroom_name, dayofweek, start_time, end_time ON le
 FOR EACH ROW
 	EXECUTE FUNCTION fn_assure_no_lecture_overlapping();
 
+-- un professore non deve avere due lezioni nello stesso intervallo orario
+CREATE OR REPLACE FUNCTION fn_assure_no_teacher_overlapping()
+RETURNS TRIGGER AS $$
+DECLARE
+	v_teacher_id integer;
+BEGIN
+	SELECT teacher_uid INTO v_teacher_id
+	FROM course
+	WHERE course_id = NEW.course_id;
+
+	IF EXISTS (
+		SELECT 1
+		FROM lecture l
+		JOIN course c ON l.course_id = c.course_id
+		WHERE c.teacher_uid = v_teacher_id
+		AND l.dayofweek = NEW.dayofweek
+		AND (TG_OP = 'INSERT' OR l.lecture_id != NEW.lecture_id)
+		AND (NEW.start_time < l.end_time AND NEW.end_time > l.start_time)
+	) THEN
+		RAISE EXCEPTION 'teacher with ID % is already occupied in another lecture on % between % and %', 
+			v_teacher_id, NEW.dayofweek, NEW.start_time, NEW.end_time;
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trg_assure_no_teacher_overlapping
+BEFORE INSERT OR UPDATE OF course_id, dayofweek, start_time, end_time ON lecture
+FOR EACH ROW
+	EXECUTE FUNCTION fn_assure_no_teacher_overlapping();
+
+-- una richiesta di spostamento lezione non puo' essere valida se l'orario e data stabiliti sono gia' impegnati per l'aula
 CREATE OR REPLACE FUNCTION fn_valid_changeofdate_req()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -118,11 +173,8 @@ BEGIN
 		FROM lecture
 		WHERE classroom_name = lec_classroom
 		AND dayofweek = new.new_dayofweek
-		AND (
-			new.new_start_time BETWEEN start_time AND end_time
-			OR
-			new.new_end_time BETWEEN start_time AND end_time
-		)
+		AND lecture_id <> new.lecture_id 
+		AND (new.new_start_time < end_time AND new.new_end_time > start_time)
 	) THEN
 		RAISE EXCEPTION 'classroom is already busy on % %-%', new.new_dayofweek, new.new_start_time, new.new_end_time;
 	END IF;
@@ -137,11 +189,16 @@ ON change_of_date_req
 FOR EACH ROW
 	EXECUTE FUNCTION fn_valid_changeofdate_req();
 
+-- un utente non coordinatore non puo' modificare lo stato di una richiesta spostamento
 CREATE OR REPLACE FUNCTION fn_check_valid_reviewer()
 RETURNS TRIGGER AS $$
 DECLARE
 	is_reviewer_coordinator teacher.is_coordinator%TYPE;
 BEGIN
+	IF new.reviewing_coord_id IS NULL THEN
+		RETURN NEW;
+	END IF;
+
 	SELECT is_coordinator INTO is_reviewer_coordinator
 	FROM teacher 
 	WHERE user_id = new.reviewing_coord_id;
