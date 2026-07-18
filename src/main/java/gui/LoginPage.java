e73f13630f6f112c3c7a60ed623c795b6a3565f8
andryrob06-glitch <andryrob06@gmail.com>
2026-07-18 18:07:05 +0200

    fix LoginPage

--- src/main/java/gui/LoginPage.java
+++ src/main/java/gui/LoginPage.java
@@ -1,7 +1,6 @@
 package gui;
 
 import controller.*;
-
 import javax.security.sasl.AuthenticationException;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -10,12 +9,11 @@ import java.awt.event.ActionListener;
 public class LoginPage {
     private static JFrame frame;
     private JPanel basePanel;
-    private JComboBox<String> roleComboBox;
     private JTextField loginTextField;
     private JPasswordField pswdField;
     private JButton loginButton;
     private JButton regButton;
-    private final UserAuthentication controller = new UserAuthentication();
+    private final UserAuthentication ua = new UserAuthentication();
 
     public static void main(String[] args) {
         frame = new JFrame("LoginPage");
@@ -36,40 +34,16 @@ public class LoginPage {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String role = (String) roleComboBox.getSelectedItem();
                 String login = loginTextField.getText();
                 String pswd = String.valueOf(pswdField.getPassword());
                 boolean success = true;
-
-                switch (role) {
-                    case "Studente":
-                        try {
-                            controller.login(login, pswd);
-                        } catch (AuthenticationException ae) {
-                            JOptionPane.showMessageDialog(basePanel, ae.getMessage());
-                            success = false;
-                        }
-                        break;
-                    case "Docente":
-                        try {
-                            controller.login(login, pswd);
-                        } catch (AuthenticationException ae) {
-                            JOptionPane.showMessageDialog(basePanel, ae.getMessage());
-                            success = false;
-                        }
-                        break;
-                    case "Coordinatore":
-                        try {
-                            controller.login(login, pswd);
-                        } catch (AuthenticationException ae) {
-                            JOptionPane.showMessageDialog(basePanel, ae.getMessage());
-                            success = false;
-                        }
-                        break;
-                    default:
-                        JOptionPane.showMessageDialog(frame, "Invalid role");
-                        break;
+                try {
+                    ua.login(login, pswd);
+                } catch (AuthenticationException ae) {
+                    JOptionPane.showMessageDialog(basePanel, ae.getMessage());
+                    success = false;
                 }
+
                 if (success) {
                     JOptionPane.showMessageDialog(basePanel, "Login Success");
                     new HomePage();
